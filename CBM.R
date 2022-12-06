a6a2453ab599fc931f85bc30b78f6e33b5908fa4
Seongho Jang <jang0212@tukorea.ac.kr>
2022-12-06 17:41:21 +0900

    12.06

--- CBM.R
+++ CBM.R
@@ -24,10 +24,10 @@ length(which(ir_t2$Tsq_mat[1:50]>ir_t2$CL))/50
 ####beta error 성능은~~
 length(which(ir_t2$Tsq_mat[51:150]<ir_t2$CL))/100
 
-mat_mat = matrix(0,100,3)
-for(i in 1:100){
-  ir_t2 = t_square(ir[1:55,],ir,i/100)
-  mat_mat[i,1] = i/100
+mat_mat = matrix(0,1000,3)
+for(i in 1:1000){
+  ir_t2 = t_square(ir[1:55,],ir,i/1000)
+  mat_mat[i,1] = i/1000
   mat_mat[i,2] = length(which(ir_t2$Tsq_mat[1:50]>ir_t2$CL))/50
   mat_mat[i,3] = length(which(ir_t2$Tsq_mat[51:150]<ir_t2$CL))/100
 }
@@ -51,26 +51,27 @@ cbm = function(trdat, tedat, alpha,k) {
     obs = nrow(s2)
     dim = ncol(trdat)
     
-    mu = colMeans(s2[,1:dim])
+    mu = colMeans(s2[,1:dim]) # 열별 평균
     
     #CL = qf(1-alpha,dim,obs - dim)*(dim*(obs+1)*(obs-1)/(obs*(obs-dim))) # bootstrap(비모수 방법), CL(모수적인 방법) 둘중 하나만 사용 
     
-    sinv = ginv(cov(s2[,1:dim]))  
+    sinv = ginv(cov(s2[,1:dim])) # 공분산 역행렬 
     
-    mu_mat = repmat(mu, nrow(tedat),1)
-    dte = tedat-mu_mat
+    mu_mat = repmat(mu, nrow(tedat),1) # 배열 복사
+    dte = tedat-mu_mat # test set에 열별 평균 빼줌
     
-    Tsq_mat = matrix(numeric(0), nrow(tedat),1) 
-    
+    Tsq_mat = matrix(numeric(0), nrow(tedat),1) # 빈 행렬 생성 
+   
+    # 마할라노비스 거리 계산 후 cbm_mat 저장 
     for( j in 1:nrow(tedat)) {
       Tsq_mat[j,1] = as.double(dte[j,]) %*% sinv %*% t(t(as.double(dte[j,])))
     }
     cbm_mat[,i] = Tsq_mat
   }
-  cbm_res = apply(cbm_mat,1,min)
-  ret = list(cbm_res=cbm_res)
+  cbm_res = apply(cbm_mat,1,min) # 행 단위로 min 값 
+  ret = list(cbm_res=cbm_res) # 
 
-  return (ret)                                    
+  return (ret)                       
 }
 
 trdat_test = cbm(trdat, tedat, 0.01,3)
@@ -97,12 +98,12 @@ s1 = bootlimit1(trdat, 0.05, 100)
 ####
 
 for(i in 1:100){
-  ir_cbm = cbm(ir[1:55,],ir,i/100,k=3)
-  cbm_boot = boot_cl(ir_cbm$cbm_res,i/100)
+  ir_cbm = cbm(ir[1:55,],ir,i/100,k=2)
+  cbm_boot = bootlimit1(ir_cbm$cbm_res,i/100,100)
   mat_mat[i,1] = i/100
-  mat_mat[i,2] = length(which(ir_cbm$cbm_res[1:50]>cbm_boot$cl))/50
-  mat_mat[i,3] = length(which(ir_cbm$cbm_res[51:150]<cbm_boot$cl))/100
+  mat_mat[i,2] = length(which(ir_cbm$cbm_res[1:50]>cbm_boot))/50
+  mat_mat[i,3] = length(which(ir_cbm$cbm_res[51:150]<cbm_boot))/100
 }
 
-points(mat_mat[,2:3],col='red',type='o')
-
+points(mat_mat[,2:3],col='red',type='l')
+tail(mat_mat)
